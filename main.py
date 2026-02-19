d893b8e881d5605f38c191b0b9f711c39a1757fa
Mauricio Araujo <mauricio.araujo@proton.me>
2026-02-19 17:15:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hellro,ss World!"}
+    return {"message": "Hellro,ss Wforld!"}
 
 @app.get("/health")
 def health_check():
